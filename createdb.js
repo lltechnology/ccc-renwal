b35e0425491ea4fca2d36f738441540e354fa046
King <King@AC8C066F.ipt.aol.com>
2017-07-22 13:10:01 +0900

    little change

--- createdb.js
+++ createdb.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://lltechnology:ll0505@ds117093.mlab.com:17093/ccc";
+var url = "mongodb://ccc-king:ll0505@ds117093.mlab.com:17093/ccc";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
+const url = process.env.MONGO_URL;
 
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://lltechnology:ll0505@ds117093.mlab.com:17093/ccc';
 
 const stripe = require("stripe")(keySecret);
 const bodyParser = require("body-parser");
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,14 +20,28 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
-app.get('/renewal', function(request, response) {
-  response.render('pages/renewal');
+app.get('/renewal', function(req, res) {
+  res.render('pages/renewal');
+});
+
+// POST /payment gets urlencoded bodies
+app.post('/payment', urlencodedParser, function(req, res) {
+
+  if (!req.body) return res.sendStatus(400)
+   res.render('pages/result', {data: req.body});
+
+  var item = {
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+  };
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log(url);
 });
