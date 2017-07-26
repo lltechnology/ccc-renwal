c7601b2c4ed161c01752495a288f56eac9b5e5cc
unknown <king@crownairsoft.com>
2017-07-26 11:47:14 +0800

    ccc

--- index.js
+++ index.js
@@ -70,7 +70,28 @@ app.post('/payment', urlencodedParser, function (req, res) {
     description: 'CCC Membership 2017-18 - ' + item.englishname + ' ()' + jerseykit + ')',
     source: token,
   }, function (err, charge) {
-    // asynchronously called
+// Error handling ======================================================
+    switch (err.type) {
+      case 'StripeCardError':
+        // A declined card error
+        err.message; // => e.g. "Your card's expiration year is invalid."
+        break;
+      case 'StripeInvalidRequestError':
+        // Invalid parameters were supplied to Stripe's API
+        break;
+      case 'StripeAPIError':
+        // An error occurred internally with Stripe's API
+        break;
+      case 'StripeConnectionError':
+        // Some kind of error occurred during the HTTPS communication
+        break;
+      case 'StripeAuthenticationError':
+        // You probably used an incorrect API key
+        break;
+      case 'StripeRateLimitError':
+        // Too many requests hit the API too quickly
+        break;
+    }
   });
 
   // Mongo Connect ==================================================================
