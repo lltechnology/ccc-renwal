9d584a90a215f8e7b401b2fc267ac775c7eef2ab
unknown <king@crownairsoft.com>
2017-07-26 01:41:29 -0700

    ccc-renewal

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
 const url = process.env.MONGO_URL;
-
 const express = require('express');
-const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 
-const stripe = require("stripe")(keySecret);
-const bodyParser = require("body-parser");
-var assert = require("assert")
+const app = express();
+
+var stripe = require('stripe')('sk_test_rnH501ygqUSnlOfpijhbWxrA');
+var assert = require('assert');
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -21,34 +21,51 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('pages/renewal');
+app.get('/', function (req, res) {
+  res.render('pages/renewal1');
 });
 
-app.get('/renewal', function(req, res) {
-  res.render('pages/renewal');
+app.get('/renewal', function (req, res) {
+  res.render('pages/renewal', { title: 'Membership Renewal' });
 });
 
 // POST /payment gets urlencoded bodies
-app.post('/payment', urlencodedParser, function(req, res) {
+app.post('/payment', urlencodedParser, function (req, res) {
 
   var item = {
     email: req.body.email,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
     password: req.body.pwd,
+    firstname: req.body.chinesename,
+    lastname: req.body.englishname,
     gender: req.body.gender,
     dob: req.body.dob,
+    hkid: req.body.hkid,
+    chinacard: req.body.chinacard,
     mobile: req.body.mobile,
     address: req.body.address,
-    district: req.body.district,
-    bibsize: req.body.bibsize,
     jerseysize: req.body.jerseysize,
+    bibsize: req.body.bibsize,
+    padtype: req.body.padtype,
     glovesize: req.body.glovesize,
-    socksize: req.body.socksize
+    socksize: req.body.socksize,
   };
 
-  MongoClient.connect(url, function(err, db) {
+  // Token is created using Stripe.js or Checkout!
+  // Get the payment token submitted by the form:
+  var token = req.body.stripeToken;   // Using Express
+
+  // Charge the user's card:
+  var charge = stripe.charges.create({
+    amount: 200000,
+    currency: 'hkd',
+    customer: req.body.firstname,
+    description: 'CCC Membership Apex',
+    source: token,
+  }, function (err, charge) {
+    // asynchronously called
+  });
+
+  MongoClient.connect('mongodb://ccc-king:ll0505@ds117093.mlab.com:17093/ccc', function(err, db) {
     assert.equal(null, err);
     db.collection('user-data').insertOne(item, function(err, result) {
       assert.equal(null, err);
@@ -64,5 +81,4 @@ app.post('/payment', urlencodedParser, function(req, res) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-  console.log(url);
 });
